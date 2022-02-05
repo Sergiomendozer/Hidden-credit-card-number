b1c68b92199d03b6731de5664f1c05e1a9d2ac55
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-04 22:06:48 -0800

    reformatted

--- hidden credit card number.py
+++ hidden credit card number.py
@@ -1,8 +1,8 @@
-credit_card_number= str(input("enter your 16 digit credit card:"))
+credit_card_number = str(input("enter your 16 digit credit card:"))
 if len(credit_card_number) > 16:
-    print ("you have entered more than 16 digits")
+    print("you have entered more than 16 digits")
 elif len(credit_card_number) < 16:
-    print ("you have entered less than 16 digits")
+    print("you have entered less than 16 digits")
 elif len(credit_card_number) == 16:
     dash = "*" * 12
-    print (dash+credit_card_number)[-4:]
+    print(dash + credit_card_number)[-4:]
